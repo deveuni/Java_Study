1761b9f3631af7fc3fd36b728641488d1a5a2496
지은 <vividdream0215@naver.com>
2021-11-04 22:10:03 +0900

    scanner

---
+++ Java_Study/src/scanner/ScannerDemo.java
@@ -0,0 +1,16 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		int i = sc.nextInt();
+		System.out.println(i*1000);
+		sc.close();
+
+	}
+
+}
